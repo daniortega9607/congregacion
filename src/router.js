6b7156903e9763bba081c27830ff2f59765a2857
Daniel Ortega <danielortega@Daniels-MacBook-Pro.local>
2019-03-18 09:12:53 -0600

    Test Pwa

--- src/router.js
+++ src/router.js
@@ -7,7 +7,8 @@ Vue.use(Router)
 export const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/about', name: 'about', component: () => import('./views/About.vue') },
-  { path: '/login', name: 'login', component: () => import('./views/Login.vue') }
+  { path: '/login', name: 'login', component: () => import('./views/Login.vue') },
+  { path: '*', redirect: '/' }
 ]
 
 export default new Router({ mode: 'history', routes })
